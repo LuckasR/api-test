2f9b09528bc912fc0f0527c9f3eccc951fdedf6f
Luckas <163956999+LuckasR@users.noreply.github.com>
2026-04-25 17:22:10 +0300

    fix

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import * as companyController from '../controllers/companyController.js';
+// import express from 'express';
+// import * as companyController from '../controllers/companyController.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/', companyController.create);
-router.get('/', companyController.getAll);
-router.get('/:id', companyController.getById);
-router.put('/:id', companyController.update);
-router.delete('/:id', companyController.remove);
+// router.post('/', companyController.create);
+// router.get('/', companyController.getAll);
+// router.get('/:id', companyController.getById);
+// router.put('/:id', companyController.update);
+// router.delete('/:id', companyController.remove);
 
-export default router;
+// export default router;
